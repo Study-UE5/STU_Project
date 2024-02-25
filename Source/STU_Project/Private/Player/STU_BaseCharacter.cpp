b3ebf1e6eafa37601030ee0988258811a1ce7410
WORK\mywor <qwastaras>
2024-02-25 17:53:41 +0000

    003 movement input 025

--- Source/STU_Project/Private/Player/STU_BaseCharacter.cpp
+++ Source/STU_Project/Private/Player/STU_BaseCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/STU_BaseCharacter.h"
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 ASTU_BaseCharacter::ASTU_BaseCharacter()
@@ -33,5 +34,21 @@ void ASTU_BaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASTU_BaseCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASTU_BaseCharacter::MoveRight);
+
+}
+
+void ASTU_BaseCharacter::MoveForward(float Amount)
+{
+	//IsMovingForward = Amount > 0.0f;
+	//if (Amount == 0.0f) return;
+	AddMovementInput(GetActorForwardVector(), Amount);
+}
+
+void ASTU_BaseCharacter::MoveRight(float Amount)
+{
+	if (Amount == 0.0f) return;
+	AddMovementInput(GetActorRightVector(), Amount);
 }
 
